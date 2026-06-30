ac3341bdc7549957c6e058c82bdff297158cd3c1
Viktor Loktev <sixtead@gmail.com>
2026-06-30 20:49:55 +0200

    add /health endpoint

--- src/main/java/org/sixtead/blog_api/MainVerticle.java
+++ src/main/java/org/sixtead/blog_api/MainVerticle.java
@@ -2,17 +2,25 @@ package org.sixtead.blog_api;
 
 import io.vertx.core.Future;
 import io.vertx.core.VerticleBase;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends VerticleBase {
 
   @Override
   public Future<?> start() {
-    return vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onSuccess(http -> {
-      System.out.println("HTTP server started on port 8888");
-    });
+    var httpServer = vertx.createHttpServer();
+    var router = Router.router(vertx);
+
+    router
+      .get("/health")
+      .respond(ctx -> Future.succeededFuture(JsonObject.of("status", "UP")));
+
+    return httpServer
+      .requestHandler(router)
+      .listen(8888)
+      .onSuccess(http -> {
+        System.out.println("HTTP server started on port 8888");
+      });
   }
 }
--- src/test/java/org/sixtead/blog_api/TestMainVerticle.java
+++ src/test/java/org/sixtead/blog_api/TestMainVerticle.java
@@ -1,14 +1,21 @@
 package org.sixtead.blog_api;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientResponse;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.json.JsonObject;
 import io.vertx.junit5.VertxExtension;
 import io.vertx.junit5.VertxTestContext;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(VertxExtension.class)
 public class TestMainVerticle {
+
+  private HttpClient client;
 
   @BeforeEach
   void deploy_verticle(Vertx vertx, VertxTestContext testContext) {
@@ -17,6 +24,13 @@ public class TestMainVerticle {
 
   @Test
   void verticle_deployed(Vertx vertx, VertxTestContext testContext) throws Throwable {
-    testContext.completeNow();
+    client = vertx.createHttpClient();
+
+    client.request(HttpMethod.GET, 8888, "localhost", "/health")
+      .compose(req -> req.send().compose(HttpClientResponse::body))
+      .onComplete(testContext.succeeding(body -> testContext.verify(() -> {
+        Assertions.assertEquals(JsonObject.of("status", "UP"), body.toJsonObject());
+        testContext.completeNow();
+      })));
   }
 }
